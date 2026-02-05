0add4234d0481ad88e24cf0d1e3db4f3d0c4fdda
Brandon T <itsbt125@users.noreply.github.com>
2026-02-05 12:59:38 -0500

    Refactor configure_system function parameters and logic

--- cfg/config.py
+++ cfg/config.py
@@ -1,6 +1,6 @@
 import subprocess
 
-def run_chroot(cmd): # Easier way to run commands via chroot.
+def run_chroot(cmd): 
     full_cmd = ["arch-chroot", "/mnt"] + cmd
     try:
         subprocess.run(full_cmd, check=True)
@@ -13,41 +13,53 @@ def generate_fstab():
     with open("/mnt/etc/fstab", "w") as f:
         subprocess.run(["genfstab", "-U", "/mnt"], stdout=f, check=True)
 
-def configure_system(hostname, username, password, timezone, locale):
-    print(f"Hostname: {hostname}")
+def configure_system(hostname, username, user_password, root_password, timezone="America/New_York", locale="en_US.UTF-8", keymap="us"):
+    print(f"--> Configuring System: {hostname}")
 
+    # Timezone
     run_chroot(["ln", "-sf", f"/usr/share/zoneinfo/{timezone}", "/etc/localtime"])
     run_chroot(["hwclock", "--systohc"])
 
+    # Console Keymap Handling
+    print(f"--> Setting Console Keymap: {keymap}")
+    with open("/mnt/etc/vconsole.conf", "w") as f:
+        f.write(f"KEYMAP={keymap}\n")
+
+    # Sets Locale
     run_chroot(["sed", "-i", f"s/^#{locale}/{locale}/", "/etc/locale.gen"])
     run_chroot(["locale-gen"])
     
     with open("/mnt/etc/locale.conf", "w") as f:
         f.write(f"LANG={locale}\n")
 
+    # Hostname
     with open("/mnt/etc/hostname", "w") as f:
         f.write(f"{hostname}\n")
     
     with open("/mnt/etc/hosts", "w") as f:
         f.write(f"127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t{hostname}.localdomain\t{hostname}\n")
 
-    ps = subprocess.Popen(["echo", f"root:{password}"], stdout=subprocess.PIPE)
+    # Root Password
+    print("Configuring Root Password...")
+    ps = subprocess.Popen(["echo", f"root:{root_password}"], stdout=subprocess.PIPE)
     subprocess.run(["arch-chroot", "/mnt", "chpasswd"], stdin=ps.stdout, check=True)
     ps.stdout.close()
 
+    # Creating a user account (root shares the same password for simplicity)
     print(f"Creating User: {username}")
     try:
         run_chroot(["useradd", "-m", "-G", "wheel,network,video,audio,storage", username])
     except:
-        print("User might already exist, setting password anyway.")
+        print("User might already exist.")
         
-    ps_user = subprocess.Popen(["echo", f"{username}:{password}"], stdout=subprocess.PIPE)
+    ps_user = subprocess.Popen(["echo", f"{username}:{user_password}"], stdout=subprocess.PIPE)
     subprocess.run(["arch-chroot", "/mnt", "chpasswd"], stdin=ps_user.stdout, check=True)
     ps_user.stdout.close()
 
+    # Adds user account to sudoers and starts NetworkManager and sddm for next boot.
     run_chroot(["sed", "-i", "s/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/", "/etc/sudoers"])
 
-    print("Enabling Services...")
+    print("--> Enabling Services...")
     services = ["NetworkManager", "sddm"]
     for s in services:
         run_chroot(["systemctl", "enable", s])
